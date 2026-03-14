57642a3d99fa59f3a3256143eea7f870a856acff
ryangpham <ryannjr2004@gmail.com>
2026-03-14 14:54:18 -0400

    chore: add restaurant and meal models

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -1,6 +1,35 @@
 class Meal {
-  const Meal({required this.id, required this.name});
+  final int? id;
+  final String mealName;
+  final String restaurantName;
+  final double price;
+  final String date;
 
-  final String id;
-  final String name;
+  Meal ({
+    this.id,
+    required this.mealName,
+    required this.restaurantName,
+    required this.price,
+    required this.date,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'mealName': mealName,
+      'restaurantName': restaurantName,
+      'price': price,
+      'date': date,
+    };
+  }
+
+  factory Map.fromMap(Map<String, dynamic> map) {
+   return Meal(
+      id: map['id'],
+      mealName: map['mealName'],
+      restaurantName: map['restaurantName'],
+      price: map['price'],
+      date: map['date'],
+    ); 
+  }
 }
