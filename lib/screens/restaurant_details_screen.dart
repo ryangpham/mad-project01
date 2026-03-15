96b9bf053da4662ae723662e228a8ab0aa4d5ed5
wildheart156 <wildheart156@yahoo.com>
2026-03-15 14:11:24 -0400

    updated restaurant_details_screen for further UI

--- lib/screens/restaurant_details_screen.dart
+++ lib/screens/restaurant_details_screen.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
+import '../models/restaurant.dart';
 
 class RestaurantDetailsScreen extends StatelessWidget {
-  const RestaurantDetailsScreen({super.key});
+  final Restaurant restaurant;
+
+  const RestaurantDetailsScreen({super.key, required this.restaurant});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Restaurant Details Screen')),
+    return Scaffold(
+      appBar: AppBar(title: Text(restaurant.name)),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Icon(Icons.restaurant, size: 80),
+
+            const SizedBox(height: 20),
+
+            Text(
+              restaurant.name,
+              style: const TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            ),
+
+            const SizedBox(height: 10),
+
+            Text("Price: ${restaurant.price}"),
+            Text("Distance: ${restaurant.distance} miles"),
+            Text("Hours: ${restaurant.hours}"),
+
+            const SizedBox(height: 30),
+
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Add to Favorites"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
